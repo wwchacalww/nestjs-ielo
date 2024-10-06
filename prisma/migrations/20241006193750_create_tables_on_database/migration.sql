06e8e6ae2699d469c9da80e107867064b45d7364
Vicente Cartaxo <juniorcartaxo@gmail.com>
2024-10-06 18:38:26 -0300

    feat: :sparkles: Criado e configurado o Banco de Dados em postgres
    Instalado e configurado o orm prisma juntamente com o schema do banco de dados

---
+++ prisma/migrations/20241006193750_create_tables_on_database/migration.sql
@@ -0,0 +1,126 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "professionals" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "birth_date" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "fone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "register" TEXT NOT NULL,
+    "specialty" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "professionals_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "patients" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "birth_date" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "fone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "payment" TEXT NOT NULL,
+    "responsible" TEXT NOT NULL,
+    "parent" TEXT NOT NULL,
+    "cpfResponsible" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "patients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "appointments" (
+    "id" SERIAL NOT NULL,
+    "specialty" TEXT NOT NULL,
+    "appointment" TIMESTAMP(3) NOT NULL,
+    "local" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "payment" TEXT NOT NULL,
+    "value" DECIMAL(65,30) NOT NULL,
+    "professional_id" TEXT NOT NULL,
+    "patient_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "appointments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "progress" (
+    "id" TEXT NOT NULL,
+    "progress" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "appointment_id" INTEGER NOT NULL,
+    "patient_id" TEXT NOT NULL,
+    "professional_id" TEXT NOT NULL,
+    "supervisor_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "progress_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reports" (
+    "id" TEXT NOT NULL,
+    "report" TEXT NOT NULL,
+    "patient_id" TEXT NOT NULL,
+    "professional_id" TEXT NOT NULL,
+    "supervisor_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "reports_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "professionals_cpf_key" ON "professionals"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_cpf_key" ON "patients"("cpf");
+
+-- AddForeignKey
+ALTER TABLE "professionals" ADD CONSTRAINT "professionals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_professional_id_fkey" FOREIGN KEY ("professional_id") REFERENCES "professionals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_patient_id_fkey" FOREIGN KEY ("patient_id") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "progress" ADD CONSTRAINT "progress_appointment_id_fkey" FOREIGN KEY ("appointment_id") REFERENCES "appointments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "progress" ADD CONSTRAINT "progress_patient_id_fkey" FOREIGN KEY ("patient_id") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "progress" ADD CONSTRAINT "progress_professional_id_fkey" FOREIGN KEY ("professional_id") REFERENCES "professionals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_patient_id_fkey" FOREIGN KEY ("patient_id") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_professional_id_fkey" FOREIGN KEY ("professional_id") REFERENCES "professionals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ readme.md
@@ -0,0 +1,8 @@
+## NESTJS - IELO
+**Repositório com objetivo de criar uma api para um consultório**
+
+  ### Banco de Dados
+ - [ ] Criar docker-compose com imagem do POSTGRES
+ - [ ] Instalar e configurar PRISMA no NESTJS
+ - [ ] Criar tabelas
+ - [ ] Criar serviço do Prisma no Nestjs
