8b817d8fd4067aa1cd34072e9ceb1e3ee9df209b
Vicente Cartaxo <juniorcartaxo@gmail.com>
2025-03-19 20:22:51 -0300

    feat:Calculo de atendimento dos cards

--- src/controllers/appointments/getMonthlyAppointmentsCount.controller.ts
+++ src/controllers/appointments/getMonthlyAppointmentsCount.controller.ts
@@ -1,8 +1,20 @@
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
 import { PrismaService } from '@/prisma/prisma.service'
 import { calculatePercentageDifference } from '@/utils/calculatePercentageDifference'
-import { Controller, Get, HttpCode, UseGuards } from '@nestjs/common'
+import { Controller, Get, HttpCode, Query, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
+import { z } from 'zod'
 
+const getMonthlyAppointmentsCountQuerySchema = z.object({
+  query: z.enum(['all', 'social', 'private', 'plan']).optional().default('all'),
+})
+
+const queryValidationPipe = new ZodValidationPipe(
+  getMonthlyAppointmentsCountQuerySchema,
+)
+type GetMonthlyAppointmentsCountQuery = z.infer<
+  typeof getMonthlyAppointmentsCountQuerySchema
+>
 @Controller('/api/appointments/monthly-appointments-count')
 @UseGuards(AuthGuard('jwt'))
 export class GetMonthlyAppointmentsCount {
@@ -10,13 +22,56 @@ export class GetMonthlyAppointmentsCount {
 
   @Get()
   @HttpCode(200)
-  async handle() {
-    const appointments = await this.prisma.appointment.findMany({
-      select: {
-        start: true,
-      },
-    })
+  async handle(
+    @Query(queryValidationPipe) query: GetMonthlyAppointmentsCountQuery,
+  ) {
+    let appointments: {
+      start: Date
+    }[] = []
+
+    if (query.query === 'all') {
+      appointments = await this.prisma.appointment.findMany({
+        select: {
+          start: true,
+        },
+      })
+    } else if (query.query === 'social') {
+      appointments = await this.prisma.appointment.findMany({
+        where: {
+          payment: 'Social',
+        },
+        select: {
+          start: true,
+        },
+      })
+    } else if (query.query === 'private') {
+      appointments = await this.prisma.appointment.findMany({
+        where: {
+          payment: 'Particular',
+        },
+        select: {
+          start: true,
+        },
+      })
+    } else if (query.query === 'plan') {
+      appointments = await this.prisma.appointment.findMany({
+        where: {
+          NOT: {
+            payment: 'Particular',
+          },
+          AND: {
+            NOT: {
+              payment: 'Social',
+            },
+          },
+        },
+        select: {
+          start: true,
+        },
+      })
+    }
     const mm = new Date().getMonth()
+
     const mmBefore = mm - 1 < 0 ? 11 : mm - 1
 
     const appointmentsCount = appointments.reduce(
@@ -27,13 +82,13 @@ export class GetMonthlyAppointmentsCount {
       },
       {} as Record<number, number>,
     )
-    const diff = calculatePercentageDifference(
-      appointmentsCount[mm],
-      appointmentsCount[mmBefore],
-    )
+
+    const now = appointmentsCount[mm] || 0
+    const before = appointmentsCount[mmBefore] || 0
+    const diff = calculatePercentageDifference(now, before)
 
     return {
-      total: appointmentsCount[mm],
+      total: appointmentsCount[mm] || 0,
       percentage: diff,
     }
   }
--- src/utils/calculatePercentageDifference.ts
+++ src/utils/calculatePercentageDifference.ts
@@ -2,6 +2,5 @@ export function calculatePercentageDifference(a: number, b: number) {
   if (b === 0) return 0
   if (a === 0) return -100
   const percentageDifference = ((a - b) / b) * 100
-  console.log(percentageDifference)
   return percentageDifference.toFixed(2) + '%'
 }
