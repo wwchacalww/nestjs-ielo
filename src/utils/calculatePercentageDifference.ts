69cb91331f8e3f5d9e15a73ab88ce7364bd4f00d
Vicente Cartaxo <juniorcartaxo@gmail.com>
2025-03-19 15:48:27 -0300

    fix:Ajuste no calculo da porcetagem

--- src/utils/calculatePercentageDifference.ts
+++ src/utils/calculatePercentageDifference.ts
@@ -1,15 +1,7 @@
 export function calculatePercentageDifference(a: number, b: number) {
   if (b === 0) return 0
   if (a === 0) return -100
-  if (a < b) {
-    const percentageDifference = ((a - b) / b) * 100
-    const diff = (percentageDifference + 100) * -1
-
-    return diff.toFixed(2) + '%'
-  }
-  if (a > b) {
-    const percentageDifference = ((a - b) / b) * 100
-    const diff = percentageDifference - 100
-    return diff.toFixed(2) + '%'
-  }
+  const percentageDifference = ((a - b) / b) * 100
+  console.log(percentageDifference)
+  return percentageDifference.toFixed(2) + '%'
 }
